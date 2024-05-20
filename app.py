44f728bc40eb645c3f9280dec59d276f98275435
Sonu <31376959+sonusingh10@users.noreply.github.com>
2024-05-20 04:55:44 +0000

    added html + CSS

--- app.py
+++ app.py
@@ -4,18 +4,21 @@
 # We are importing the class and we r creating object of the class
 #https://www.youtube.com/watch?v=yBDHkveJUf4
 #https://jovian.com/learn/web-development-with-python-and-flask
-#hmtldog.com , if you want to lean hlm 
-#excalidraw.com 
+#hmtldog.com , if you want to lean hlm
+#excalidraw.com
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-  return "Hello Juhi Singh , how you are doing Good morning"
-  #return render_template('home.html')
+  #return "Hello Juhi Singh , how you are doing Good morning :) This message from Sonu Singh"
+  return render_template('home.html')
+
 
 if __name__ == "__main__":
   print("I am inside if")
-  
+
   app.run(host="0.0.0.0", debug=True)
