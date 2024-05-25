6d48175737b5f353df7420f49166da0934b75baf
Sonu <31376959+sonusingh10@users.noreply.github.com>
2024-05-25 09:23:29 +0000

    fix issue + add link

--- app.py
+++ app.py
@@ -6,8 +6,7 @@
 #https://jovian.com/learn/web-development-with-python-and-flask
 #hmtldog.com , if you want to lean hlm
 #excalidraw.com
-
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -17,39 +16,35 @@ JOBS = [
     'title': 'Data Analyst',
     'location': 'Bengaluru, India',
     'salary': 'Rs. 10,00,000'
-  }, 
+  },
   {
     'id': 2,
     'title': 'Data Scientist',
     'location': 'Delhi, India',
     'salary': 'Rs. 15,00,000'
-}, 
-{
+  },
+  {
     'id': 3,
     'title': 'Frontend Engineer',
-    'location': 'Remote',
-    'salary': 'Rs. 12,00,000'
-}, 
-{
+    'location': 'Remote'
+  },
+  {
     'id': 4,
     'title': 'Backend Engineer',
     'location': 'San Francisco, USA',
-    'salary': '$120,000'
-}
+    'salary': '$150,000'
+  }
 ]
 
-
 @app.route("/")
-def hello_world():
-  return render_template('home.html', jobs=JOBS)
-  #return "Hello Juhi Singh , how you are doing Good morning :) This message from Sonu Singh"
+def hello_jovian():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Sonu Singh')
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
-
-if __name__ == "__main__":
-  print("I am inside if")
-
-  app.run(host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
